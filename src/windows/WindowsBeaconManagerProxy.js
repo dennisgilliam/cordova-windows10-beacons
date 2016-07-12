dc618d8c8699f749192a58a1e23f3daa9e3f08c6
Dennis Mosemann <dennis.mosemann@ambient-innovation.com>
2016-07-12 14:21:01 +0200

    test

--- src/windows/WindowsBeaconManagerProxy.js
+++ src/windows/WindowsBeaconManagerProxy.js
@@ -1,3 +1,4 @@
+/*
 //  JavaScript
 var cordova = require('cordova');
 
@@ -6,12 +7,26 @@ cordova.commandProxy.add("WindowsBeaconManager",{
     DetectBeacon:function(successCallback,errorCallback,beacons) {
         successCallback("echo");
 
-        /*
+
         if(!strInput || !strInput.length) {
             errorCallback("Error, something was wrong with the input string. =>" + strInput);
         }
         else {
             successCallback(strInput + "echo");
-        }*/
+        }
     }
 });
+*/
+
+//  JavaScript
+var cordova = require('cordova'),
+    WindowsBeaconManager = require('./WindowsBeaconManager');
+
+module.exports = {
+
+    DetectBeacon: function (successCallback, errorCallback, strInput) {
+        successCallback("echo");
+    }
+};
+
+require("cordova/exec/proxy").add("WindowsBeaconManager", module.exports);
