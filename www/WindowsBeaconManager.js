5adcb5f9a3887635affb112874a6fed5bb6eb9b3
Dennis Mosemann <dennis.mosemann@ambient-innovation.com>
2016-07-12 13:51:51 +0200

    test2

--- www/WindowsBeaconManager.js
+++ www/WindowsBeaconManager.js
@@ -7,8 +7,9 @@ var WindowsBeaconManager = {
 
 module.exports.WindowsBeaconManager = WindowsBeaconManager;
 
+/*
 window.DetectBeacon = function(str, callback) {
     cordova.exec(callback, function(err) {
         callback('Nothing to echo.');
     }, "WindowsBeaconManager", "DetectBeacon", [str]);
-};
+};*/
